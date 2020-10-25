ee3c112fc33dabbe7b27bb7cae63ad9e12a65ca3
Mark John A. Velmonte <65215676+callmemark@users.noreply.github.com>
2020-10-25 17:17:47 +0800

    GUI and Main program update
    Added new feature on gui. 
    -- single image anomaly detection
    -- check dataset
    
    Fix bugs.
    Remove unnecessary  methods.
    Added new methods

--- Program/Starguard.py
+++ Program/Starguard.py
@@ -6,7 +6,6 @@ try:
 	from skimage.feature import hog
 	from skimage.io import imread
 	from skimage import data, exposure
-	from astropy.io import fits
 	from sklearn.neighbors import KNeighborsClassifier
 	from sklearn.model_selection import train_test_split
 	from sklearn import metrics
@@ -20,13 +19,20 @@ except Exception as error:
 	print("if you think this is a bug please open a new issue in project repository")
 
 
-
 class StarGuard():
 	def __init__(self, image_size = 200, K = 3):
 		self.image_size = image_size
 		self.data_loop_count = 20
 		self.neighbors = K
 
+
+
+	def br(self):
+		print("")
+		print(("---" * 10) + ("END") + ("---" * 10))
+		print("")
+		print("")
+		print("")
 
 
 	def knnAlgorithmDetector(self, dataset, frame):
@@ -40,9 +46,31 @@ class StarGuard():
 
 
 
-	def testDatasetAccurracy(self):
-		X = self.dataset.iloc[:, :-1].values
-		y = self.dataset["classification"]
+	def help(self):
+		print("""list of methods:
+			knnAlgorithmDetector(dataset name (.csv), image dir) >>> Return a prediction
+			testDatasetAccuracy(dataset name (.csv)) >>> print the accuracy of the dataset
+			showDataset(dataset name (.csv)) >>> print the dataset
+			encodeHog(image dir) >>> return a hod encoded image
+			toOneDimArray(multidimensional numpy array) >>> return a flattened version of array
+			createImageHogDataset(dataset name (.csv), image dir) >>> create a dataset file
+			addDataToImageHogDataset(dataset name (.csv), image dir, bool) >>> add data to the existing dataset
+			createDatasetByCV(dataset name (.csv), add_data = bool, data_classification = "normal" or "anomaly str) >>> create or add new data to a dataset
+			anomalyDetectByHogImage(image dir, dataset name (.csv)) >>> print a prediction. to exit this method just click the camera viewport and click the x button on keyboard
+			anomalyDetectByCV(dataset name (.csv), cap_anomaly = bool, print_frame_class = bool, absolute_changes_detection = bool) >>> print a prediction. this method will open the cameraand will capture the image if parameter is true. to exit this method just click the camera viewport and click the x button on keyboard
+			""")
+		self.br()
+
+
+
+
+	def testDatasetAccuracy(self, dataset):
+		print("loading dataset")
+		dataset = pd.read_csv(dataset)
+
+		print("load complete", "calculating....")
+		X = dataset.iloc[:, :-1].values
+		y = dataset["classification"]
 
 		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 		knn = KNeighborsClassifier(n_neighbors = self.neighbors)
@@ -51,6 +79,7 @@ class StarGuard():
 
 		print(y_pred)
 		print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+		self.br()
 
 
 
@@ -58,6 +87,7 @@ class StarGuard():
 		print("loading dataset")
 		loaded_dataset = pd.read_csv(dataset)
 		print(loaded_dataset)
+		self.br()
 
 
 
@@ -106,7 +136,9 @@ class StarGuard():
 
 			dataset.to_csv(dataset_name, index = False)
 			print("newdataset added")
-			
+
+			self.br()
+		
 		except Exception as error:
 			print("Error:" +  str(error))
 
@@ -149,142 +181,11 @@ class StarGuard():
 			dataset.to_csv(dataset_name, index = False, header=None, mode='a')
 			print("newdataset added")
 
+			self.br()
+
 		except Exception as error:
 			print(str(error))
-
-
-
-	def createDatasetFromFits(self, dataset_name,fits_image):
-		try:
-			fits_image = fits_image
-			fits_data, header = fits.getdata(fits_image, ext=0, header = True)
-
-			fits_array = np.array(fits_data[0])
-			array_shape = fits_array.shape
-
-			array_value_count = array_shape[0]
-
-			fits_array.reshape(1, array_value_count)
-
-			loop_count = self.data_loop_count
-			fits_dataset = np.array([])
-			while loop_count > 0:
-				fits_dataset = np.append(fits_dataset, fits_array)
-				loop_count -= 1
-				print("looped" + str(loop_count))
-
-				if loop_count == 0:
-					break
-
-			print(fits_dataset.shape)
-
-			fits_dataset = fits_dataset.reshape(self.data_loop_count, int(array_value_count / 2) * 2)
-
-			print("shape" + str(fits_dataset.shape))
-			dataset = pd.DataFrame(fits_dataset, columns = map(str, range(int(array_value_count / 2) * 2)))
-			dataset["classification"] = "normal"
-
-			dataset.to_csv(dataset_name, index = False)
-			print("newdataset added")
-
-
-		except Exception as error:
-			print("Error:" + str(error))
-
-
-
-	def addDataToFitsDataSet(self, target_dataset, fits_files, normal, count = 20):
-		fits_lits = np.array(fits_files)
-		new_data_dimension = fits_lits.ndim
-		if isinstance(normal, bool) and new_data_dimension == 1:
-			try:
-				list_count = fits_lits.shape[0]
-
-				for fits_image in fits_files:
-					current_file = fits_image
-					fits_data, header = fits.getdata(current_file, ext=0, header = True)
-
-					fits_array = np.array(fits_data[0])
-					array_shape = fits_array.shape
-
-					array_value_count = array_shape[0]
-
-					fits_array.reshape(1, array_value_count)
-
-					loop_count = self.data_loop_count
-					fits_dataset = np.array([])
-					while loop_count > 0:
-						fits_dataset = np.append(fits_dataset, fits_array)
-						loop_count -= 1
-						print("looped" + str(loop_count))
-
-						if loop_count == 0:
-							break
-
-					print(fits_dataset.shape)
-					fits_dataset = fits_dataset.reshape(self.data_loop_count, int(array_value_count / 2) * 2)
-
-					print("shape" + str(fits_dataset.shape))
-					dataset = pd.DataFrame(fits_dataset, columns = map(str, range(int(array_value_count / 2) * 2)))
-
-					if normal == True:
-						dataset["classification"] = "normal"
-					elif normal == False:
-						dataset["classification"] = "anomaly"
-					else:
-						print("runtime error")
-
-					dataset.to_csv(target_dataset, index = False, header=None, mode='a')
-					print("newdataset added")
-				print("all data saved")
-
-			except Exception as error:
-				print(str(error))
-		else:
-			print("error check parameter")	
-		
-
-
-
-	def createImageDatsaset(self, dataset_name, colored_image):
-		if True:
-			resized_image = Image.open(colored_image)
-			resized_image = resized_image.resize((self.image_size,self.image_size),Image.ANTIALIAS)
-			resized_image.save("coloredImageResized.jpg",optimize=True,quality=100)
-
-			img = Image.open("coloredImageResized.jpg").convert('LA')
-			img.save('greyscale_encoded.png')
-			image = imread("greyscale_encoded.png")
-
-			print(image.ndim)
-
-			flattent_image_array = self.toOneDimArray(image)
-
-			array_value_count = flattent_image_array.shape[0]
-			dataset_array = np.array([])
-			loop_count = self.data_loop_count
-
-			while loop_count > 0:
-				dataset_array = np.append(dataset_array, flattent_image_array)
-				loop_count -= 1
-				print("looped" + str(loop_count))
-
-				if loop_count == 0:
-					break
-
-			print(dataset_array.shape)
-			dataset_array = dataset_array.reshape(self.data_loop_count, int(array_value_count / 2) * 2)
-
-			print("shape" + str(dataset_array.shape))
-			dataset = pd.DataFrame(dataset_array, columns = map(str, range(int(array_value_count / 2) * 2))) 
-			dataset["classification"] = "normal"
-
-			dataset.to_csv(dataset_name, index = False)
-			print("newdataset added")
-
-		else:
-			print("good")
-
+	
 
 
 	def createDatasetByCV(self, dataset_name, add_data = False, data_classification = "normal"):
@@ -295,59 +196,65 @@ class StarGuard():
 		    exit()
 
 		while True:
-		    ret, frame = cap.read()
-		    if not ret:
-		        print("Can't receive frame (stream end?). Exiting ...")
-		        break
+			ret, frame = cap.read()
+			if not ret:
+				print("Can't any receive frame (stream end?). Exiting ...")
+				break
 
-		    rescaled_frame = cv.resize(frame, (self.image_size, self.image_size))
-		    normal_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-		    detector_frame = cv.cvtColor(rescaled_frame, cv.COLOR_BGR2GRAY)
+			rescaled_frame = cv.resize(frame, (self.image_size, self.image_size))
+			normal_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+			detector_frame = cv.cvtColor(rescaled_frame, cv.COLOR_BGR2GRAY)
 
-		    cv.imshow('Detector Frame', detector_frame)
-		    cv.imshow('normal viewport', normal_frame)
+			cv.imshow('Detector Frame', detector_frame)
+			cv.imshow('normal viewport', normal_frame)
 
-		    if cv.waitKey(1) == ord('q'):
-		        break
+			if cv.waitKey(1) == ord('x'):
+				break
 
-		    elif cv.waitKey(1) == ord('c'):
-		    	print("please wait")
-		    	cv.imwrite("cvcaptureddata.png", detector_frame)
+			elif cv.waitKey(1) == ord('c'):
+				print("please wait")
+				cv.imwrite("cvcaptureddata.jpg", detector_frame)
 
-		    	print("---" * 20)
+				print("---" * 20)
 
-		    	flattent_image_array = self.toOneDimArray(detector_frame)
-		    	array_value_count = flattent_image_array.shape[0]
-		    	dataset_array = np.array([])
-		    	loop_count = self.data_loop_count
+				flattent_image_array = self.toOneDimArray(detector_frame)
+				array_value_count = flattent_image_array.shape[0]
+				dataset_array = np.array([])
+				loop_count = self.data_loop_count
 
-		    	while loop_count > 0:
-		    		dataset_array = np.append(dataset_array, flattent_image_array)
-		    		loop_count -= 1
-		    		print("looped" + str(loop_count))
+				while loop_count > 0:
+					dataset_array = np.append(dataset_array, flattent_image_array)
+					loop_count -= 1
+					print("looped" + str(loop_count))
 
-		    		if loop_count == 0:
-		    			break
+					if loop_count == 0:
+						break
 
-		    	print(dataset_array.shape)
-		    	dataset_array = dataset_array.reshape(self.data_loop_count, int(array_value_count / 2) * 2)
-		    	print("shape" + str(dataset_array.shape))
-		    	dataset = pd.DataFrame(dataset_array, columns = map(str, range(int(array_value_count / 2) * 2))) 
+				print(dataset_array.shape)
+				dataset_array = dataset_array.reshape(self.data_loop_count, int(array_value_count / 2) * 2)
+				print("shape" + str(dataset_array.shape))
+				dataset = pd.DataFrame(dataset_array, columns = map(str, range(int(array_value_count / 2) * 2))) 
 
-		    	if add_data == True:
-			    	dataset["classification"] = data_classification
-			    	dataset.to_csv(dataset_name, index = False, header=None, mode='a')
-			    	print("newdata added to dataset")
+				if add_data == True:
+					dataset["classification"] = data_classification
+					dataset.to_csv(dataset_name, index = False, header=None, mode='a')
+					print("newdata added to dataset")
+					self.br()
 
-		    	elif add_data == False:
-			    	dataset["classification"] = "normal"
-			    	dataset.to_csv(dataset_name, index = False)
-			    	print("newdataset added")
-		    	break
+				elif add_data == False:
+					dataset["classification"] = "normal"
+					dataset.to_csv(dataset_name, index = False)
+					print("newdataset added")
+					self.br()
+				break
 
 
-	def AnomalyDetectByHogImage(self, image):
+
+	def anomalyDetectByHogImage(self, image, dataset):
 		try:
+			print("loacding dataset")
+			dataset = pd.read_csv(dataset)
+
 			resized_image = Image.open(image)
 			resized_image = resized_image.resize((self.image_size,self.image_size),Image.ANTIALIAS)
 			resized_image.save("deleteThisAfterDatasetIsCreated.jpg",optimize=True,quality=100)
@@ -356,20 +263,22 @@ class StarGuard():
 			hog_encoded_image = self.encodeHog(image)
 			image_hog_data = np.array(hog_encoded_image).reshape(1, self.image_size * self.image_size)
 
-			X = self.dataset.iloc[:, :-1].values
-			y = self.dataset["classification"]
+			X = dataset.iloc[:, :-1].values
+			y = dataset["classification"]
 
 			knn = KNeighborsClassifier(n_neighbors = self.neighbors)
 			knn.fit(X, y)
 			prediction = knn.predict(image_hog_data)
 			print(prediction)
 
+			self.br()
+
 		except Exception as error:
 			print(str(error))
 
 
 
-	def anomalyDetectByCV(self, dataset, cap_anomaly = True, absolute_changes_detection = False):
+	def anomalyDetectByCV(self, dataset, cap_anomaly = True, print_frame_class = True, absolute_changes_detection = False):
 		try:
 			print("loading dataset")
 			print("---" * 20)
@@ -378,64 +287,70 @@ class StarGuard():
 			self.normal_absolute_data = np.array(output.head(1))[0][0:-1]
 			print("dataset loaded complete")
 			print("---" * 20)
+
+			cap = cv.VideoCapture(0)
+
+			if not cap.isOpened():
+			    print("Cannot open camera")
+			    exit()
+
+			count = 0
+			while True:
+			    ret, frame = cap.read()
+			    if not ret:
+			        print("Can't receive frame (stream end?). Exiting ...")
+			        break
+
+			    rescaled_frame = cv.resize(frame, (self.image_size, self.image_size))
+			    normal_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+			    detector_frame = cv.cvtColor(rescaled_frame, cv.COLOR_BGR2GRAY)
+
+			    flattent_image_array = self.toOneDimArray(detector_frame)
+
+			    if absolute_changes_detection == False:
+			    	prediction = self.knnAlgorithmDetector(dataset, [flattent_image_array])
+			    elif absolute_changes_detection == True:
+			    	comparison = self.normal_absolute_data == flattent_image_array
+			    	if False in comparison:
+			    		prediction = "anomaly"
+			    	else:
+			    		prediction = "normal"
+
+			    else:
+			    	print("parameter error")
+			    	break
+
+			    if prediction == "anomaly":
+			    	if print_frame_class == True:
+			    		print("captured anomaly")
+
+			    	if cap_anomaly == True:
+				    	if not os.path.exists("anomalyImages"):
+				    		os.mkdir("anomalyImages")
+				    		print("folder created")
+
+				    	count += 1
+				    	cv.imwrite("anomalyImages/" + str(time.time()) + ".jpg" , detector_frame)
+
+			    elif prediction == "normal":
+			    	if print_frame_class == True:
+			    		print("normal")
+
+			    cv.imshow('Detector Frame', detector_frame)
+			    cv.imshow('normal viewport', normal_frame)
+
+			    if cv.waitKey(1) == ord('x'):
+			    	break
+			
+			cap.release()
+			cv.destroyAllWindows()
+
+			self.br()
+
+
 		except Exception as error:
-			print("something wrong occured while reading dataset" + str(error))
+			print("Error:" + str(error))
+			self.br()
 
-		cap = cv.VideoCapture(0)
-
-		if not cap.isOpened():
-		    print("Cannot open camera")
-		    exit()
-
-		count = 0
-		while True:
-		    ret, frame = cap.read()
-		    if not ret:
-		        print("Can't receive frame (stream end?). Exiting ...")
-		        break
-
-		    rescaled_frame = cv.resize(frame, (self.image_size, self.image_size))
-		    normal_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-		    detector_frame = cv.cvtColor(rescaled_frame, cv.COLOR_BGR2GRAY)
-
-		    flattent_image_array = self.toOneDimArray(detector_frame)
-
-		    if absolute_changes_detection == False:
-		    	prediction = self.knnAlgorithmDetector(dataset, [flattent_image_array])
-		    elif absolute_changes_detection == True:
-		    	comparison = self.normal_absolute_data == flattent_image_array
-		    	if False in comparison:
-		    		prediction = "anomaly"
-		    	else:
-		    		prediction = "normal"
-
-		    else:
-		    	print("parameter error")
-		    	break
-
-		    if prediction == "anomaly":
-		    	print("captured anomaly")
-
-		    	if cap_anomaly == True:
-			    	if not os.path.exists("anomalyImages"):
-			    		os.mkdir("anomalyImages")
-			    		print("folder created")
-
-			    	count += 1
-			    	cv.imwrite("anomalyImages/" + str(time.time()) + ".png" , detector_frame)
-
-		    elif prediction == "normal":
-		    	print("normal")
-
-		    cv.imshow('Detector Frame', detector_frame)
-		    cv.imshow('normal viewport', normal_frame)
-
-		    if cv.waitKey(1) == ord('x'):
-		    	flattent_image_array = self.toOneDimArray(detector_frame)
-		    	print(flattent_image_array.ndim)
-		    	print(flattent_image_array.shape)
-		    	break
-		
-		cap.release()
-		cv.destroyAllWindows()
-
+if __name__ == "__main__":
+	module = StarGuard()
--- Program/Starguard_GUI.py
+++ Program/Starguard_GUI.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, Frame, Entry, OptionMenu, StringVar, Label
+from tkinter import Tk, Button, Frame, Entry, OptionMenu, StringVar, Label, END
 import sys
 import Starguard as stg
 
@@ -6,9 +6,15 @@ import Starguard as stg
 class MainApp():
 	def __init__(self):
 		self.root = Tk()
+		self.root.title("StarGuard")
 		self.stg = stg.StarGuard()
 		self.nav_area_bgcolor = "#dbdbdb"
 		self.inteface_frame_bgcolor = "#f2f2f2"
+
+		self.intro_text = """StarGuard is research project to demonstrate how machine learning
+can be use for astronomy
+For documentation please proceed to the project repository"""
+		self.version = "0.1.6 on 10/24/2020"
 
 
 	def showMainFrames(self):
@@ -18,16 +24,26 @@ class MainApp():
 		self.nav_buttons_frame = Frame(main_frame, height = "200px", width = "100px", bg = self.nav_area_bgcolor)
 		self.nav_buttons_frame.place(x = "0px", y = "0px")
 
-		cv_option_btn = Button(self.nav_buttons_frame, text = "cv detect", width = 16, bd = "0px", command = lambda: self.cvDetection(	))
+		cv_option_btn = Button(self.nav_buttons_frame, text = "cv detect", width = 16, bd = "0px", activebackground = "red",command = lambda: self.cvDetection())
 		cv_option_btn.place(x = "5px", y = "5px")
 
-		exit_btn = Button(self.nav_buttons_frame, text = "Exit", width = 16, bd = "0px", bg = "red", command = lambda: self.closeProgram())
+		single_image_detection_btn = Button(self.nav_buttons_frame, text = "image detect", width = 16, bd = "0px", activebackground = "red",command = lambda: self.singleImageAnomaly())
+		single_image_detection_btn.place(x = "5px", y = "25px")
+
+		tools_btn = Button(self.nav_buttons_frame, text = "Tools", width = 16, bd = "0px", activebackground = "red",command = lambda: self.tools())
+		tools_btn.place(x = "5px", y = "45px")
+
+		exit_btn = Button(self.nav_buttons_frame, text = "Exit", width = 16, bd = "0px", bg = "red", activebackground = "red",command = lambda: self.closeProgram())
 		exit_btn.place(x = "5px", y = "180px")
 
 
 		self.inteface_frame = Frame(main_frame, height = "200px", width = "300px", bg = self.inteface_frame_bgcolor)
 		self.inteface_frame.place(x = "100px", y = "0px")
 
+		Label(self.inteface_frame, text = self.intro_text, justify = "left").place(x = "5px", y ="20px")
+		Label(self.inteface_frame, text = "v." + self.version).place(x = "5px", y = "170px")
+
+		self.root.resizable(0,0)
 		self.root.mainloop()
 
 
@@ -38,6 +54,7 @@ class MainApp():
 		Label(self.cv_detection_frame, text = "dataset:").place(x = "5", y = "5px")
 		cv_dataset = Entry(self.cv_detection_frame)
 		cv_dataset.place(x = "90px", y = "5px")
+		cv_dataset.insert(END, "*.csv", )
 
 		cap_anomaly = StringVar(self.cv_detection_frame)
 		cap_anomaly.set("true")
@@ -53,7 +70,7 @@ class MainApp():
 		abs_detect_option_menu = OptionMenu(self.cv_detection_frame, abs_detect, "false", "true")
 		abs_detect_option_menu.place(x = "90px", y = "40px")
 
-		start_detection = Button(self.cv_detection_frame, text = "start detection", command = lambda: startDetection())
+		start_detection = Button(self.cv_detection_frame, text = "start detection", activebackground = "red",command = lambda: startDetection())
 		start_detection.place(x = "220px", y = "5px")
 
 
@@ -63,6 +80,7 @@ class MainApp():
 		Label(self.cv_detection_frame, text = "New datset name").place(x = "5px", y = "100px")
 		new_dataset_name = Entry(self.cv_detection_frame)
 		new_dataset_name.place(x = "90px", y = "100px")
+		new_dataset_name.insert(END, "*.csv", )
 
 		add_data_bool = StringVar(self.cv_detection_frame)
 		add_data_bool.set("false")
@@ -79,7 +97,7 @@ class MainApp():
 		data_class_option_menu.place(x = "90px", y = "140px")
 
 
-		crete_dataset_btn = Button(self.cv_detection_frame, text = "Create Dataset", command = lambda: createCvDataset())
+		crete_dataset_btn = Button(self.cv_detection_frame, text = "Create Dataset", activebackground = "red",command = lambda: createCvDataset())
 		crete_dataset_btn.place(x = "220px", y = "100px")
 
 		def startDetection():
@@ -102,16 +120,104 @@ class MainApp():
 
 		def createCvDataset():
 			new_dataset_name_value = new_dataset_name.get()
-			add_data_bool_value = add_data_bool.get()
-			if add_data_bool_value == "true":
-				add_data = True
-			elif add_data_bool_value == "false":
-				add_data = False
-			data_class_value = data_class.get()
 
-			self.stg.createDatasetByCV(new_dataset_name_value, add_data, data_class_value)
+			if len(new_dataset_name_value) <= 4:
+				print("datset name is too short")
+			else:
+				add_data_bool_value = add_data_bool.get()
+				if add_data_bool_value == "true":
+					add_data = True
+				elif add_data_bool_value == "false":
+					add_data = False
+				data_class_value = data_class.get()
+
+				self.stg.createDatasetByCV(new_dataset_name_value, add_data, data_class_value)
 
 
+	def singleImageAnomaly(self):
+		self.single_image_frame = Frame(self.inteface_frame, height = "200px", width = "300px", bg = self.inteface_frame_bgcolor)
+		self.single_image_frame.place(x = "0px", y = "0px")
+
+		Label(self.single_image_frame, text = "sample image:").place(x = "0px", y = "5px")
+		test_image = Entry(self.single_image_frame)
+		test_image.place(x = "70px", y = "5px")
+
+		Label(self.single_image_frame, text = "datset:").place(x = "0px", y = "25pxpx")
+		dataset = Entry(self.single_image_frame)
+		dataset.place(x = "70px", y = "25px")
+		dataset.insert(END, "*.csv")
+
+		predict_button = Button(self.single_image_frame, text = "start prediction", activebackground = "red", command = lambda: startPrediction())
+		predict_button.place(x = "220px", y = "5px")
+
+
+		Label(self.single_image_frame, text = "Create Dataset").place(x = "0px", y = "60px")
+		Label(self.single_image_frame, text = "image:").place(x = "0px", y = "80px")
+		new_data_image = Entry(self.single_image_frame)
+		new_data_image.place(x = "70px", y = "80px")
+
+		Label(self.single_image_frame, text = "datset name:").place(x = "0px", y = "100px")
+		new_dataset = Entry(self.single_image_frame)
+		new_dataset.place(x = "70px", y = "100px")
+		new_dataset.insert(END, "*.csv")
+
+		image_class = StringVar(self.single_image_frame)
+		image_class.set("normal")
+
+		Label(self.single_image_frame, text = "Image class:").place(x = "5px", y = "120px")
+		image_class_option_menu = OptionMenu(self.single_image_frame, image_class, "normal", "anomaly")
+		image_class_option_menu.place(x = "70px", y = "120px")
+
+		dataset_bool = StringVar(self.single_image_frame)
+		dataset_bool.set("true")
+
+		Label(self.single_image_frame, text = "new dataset:").place(x = "5px", y = "145px")
+		dataset_bool_option_menu = OptionMenu(self.single_image_frame, dataset_bool, "true", "false")
+		dataset_bool_option_menu.place(x = "70px", y = "145px")
+
+		edit_dataset_button = Button(self.single_image_frame, text = "process datset", activebackground = "red", command = lambda: processDataset())
+		edit_dataset_button.place(x = "220px", y = "80px")
+
+		def processDataset():
+			dataset_bool_input = dataset_bool.get()
+			new_dataset_input =  new_dataset.get()
+			new_data_image_input = new_data_image.get()
+			image_class_input = image_class.get()
+
+			if image_class_input == "normal":
+				normal = True
+			elif image_class_input == "anomaly":
+				normal = False
+
+			if dataset_bool_input == "true":
+				self.stg.createImageHogDataset(new_dataset_input, new_data_image_input)
+
+			elif dataset_bool_input == "false":
+				self.stg.addDataToImageHogDataset(new_dataset_input, new_data_image_input, normal)
+
+
+		def startPrediction():
+			test_image_input = test_image.get()
+			dataset_input = dataset.get()
+
+			self.stg.AnomalyDetectByHogImage(test_image_input, dataset_input)
+
+
+
+	def tools(self):
+		self.tools_frame = Frame(self.inteface_frame, height = "200px", width = "300px", bg = self.inteface_frame_bgcolor)
+		self.tools_frame.place(x = "0px", y = "0px")
+
+		Label(self.tools_frame, text = "dataset").place(x = "5px", y = "5px")
+		dataset_entry = Entry(self.tools_frame)
+		dataset_entry.place(x = "70px", y = "5px")
+
+
+		view_dataset_accuracy_btn = Button(self.tools_frame, text = "check accuracy", command = lambda: self.stg.testDatasetAccuracy(dataset_entry.get()))
+		view_dataset_accuracy_btn.place(x = "5px", y = "25px")
+
+		view_dataset_btn = Button(self.tools_frame, text = "Read dataset", command = lambda: self.stg.showDataset(dataset_entry.get()))
+		view_dataset_btn.place(x = "80px", y = "25px")
 
 
 
@@ -123,7 +229,6 @@ class MainApp():
 
 			
 
-
-app = MainApp()
-app.showMainFrames()
-	
+if __name__ == "__main__":
+	app = MainApp()
+	app.showMainFrames()
